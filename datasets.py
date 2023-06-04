0f26e295415af3e1db38bc3415009ca50f08e80e
1ceOrb <aaronmzhu@gmail.com>
2023-06-03 21:55:50 -0500

    updated dataloader

--- datasets.py
+++ datasets.py
@@ -1,74 +1,109 @@
-import csv
-import numpy as np
-import os
+from torch.utils.data import Dataset
 from PIL import Image
-from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import os
+from torchvision.io import read_image
 
-class C100Dataset:
-    """
-    X is a feature vector
-    Y is the predictor variable
-    """
-    tr_x = None  # X (data) of the training set.
-    tr_y = None  # Y (label) of the training set.
-    ts_x = None  # X (data) of the test set.
-    ts_y = None  # Y (label) of the test set.
-    label_encoder = None  # Label encoder object
+class C100Dataset(Dataset):
 
-    def __init__(self, trainfile, testfile):
+    tr_x =[]
+    tr_y =[]
+    ts_x =[]
+    ts_y =[]
+    classes = {}
 
-        if not os.path.isfile(trainfile) or not os.path.isfile(testfile):
-            raise FileNotFoundError("File '{}' not found.".format(trainfile))
-        
-        tr_x = []  # List to store training images
-        tr_y = []  # List to store training labels
-        ts_x = []  # List to store test images
-        ts_y = []  # List to store test labels
+    def __init__(self, csv_file, root_dir, transform=None, target_transform=None, train = False, test = False):
+        """
+        Args:
+            csv_file (string): Path to the CSV file containing image paths and labels.
+            root_dir (string): Root directory containing the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.data_info = pd.read_csv(csv_file, header=None)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        pd.read_csv(csv_file)
 
-        with open(trainfile, 'r') as csvfile:
-            csv_reader = csv.reader(csvfile)
-            for row in csv_reader:
-                if 'train' in row[0]:
-                    img_path, label = row[0], row[1]
-                    img = np.asarray(Image.open(os.path.join("dataset", img_path)))
-                    tr_x.append(img)
-                    tr_y.append(label)
+        # if train is true store it in tr_x and tr_y for all the images and labels in csv file
+        if train is True:
+            self.tr_x = self.data_info.iloc[:, 0]
+            self.tr_y = self.data_info.iloc[:, 1]
+
+        if test is True:
+            self.ts_x = self.data_info.iloc[:, 0]
+            self.ts_y = self.data_info.iloc[:, 1]
+
+        # Create classes / build more classes
+        self._createClassDict()
+
+    def __len__(self):
+        return len(self.data_info)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.data_info.iloc[index, 0])
+        image = read_image(img_path)
+        label = self.data_info.iloc[index, 1]
 
 
-        with open(testfile, 'r') as csvfile:
-            csv_reader = csv.reader(csvfile)
-            for row in csv_reader:
-                if 'test' in row[0]:
-                    img_path, label = row[0], row[1]
-                    img = np.asarray(Image.open(os.path.join("dataset", img_path)))
-                    ts_x.append(img)
-                    ts_y.append(label)
+        if self.transform:
+            image = self.transform(image)
 
+        label = self.classes[label]
 
-        # Convert the lists to numpy arrays
-        self.tr_x = np.array(tr_x)
-        self.tr_y = np.array(tr_y)
-        self.ts_x = np.array(ts_x)
-        self.ts_y = np.array(ts_y)
+        if self.target_transform:
+            label = self.target_transform(label)
 
-        self.label_encoder = LabelEncoder()
-        self.label_encoder.fit(np.concatenate((self.tr_y, self.ts_y), axis=0))
-        self.tr_y = self.label_encoder.transform(self.tr_y)
-        self.ts_y = self.label_encoder.transform(self.ts_y)
-
-        # Print the shapes of the training and test sets
-        print("Test set shape: {}".format(self.ts_x.shape))
-        print("Test labels shape: {}".format(self.ts_y.shape))
-        print("Training set shape: {}".format(self.tr_x.shape))
-        print("Training labels shape: {}".format(self.tr_y.shape))
-
+        return image, label
+    
+    def _createClassDict(self):
+        unique_labels = self.data_info.iloc[:, 1].unique()
+        for i, label in enumerate(unique_labels):
+            self.classes[label] = i
+    
+    def getClasses(self):
+        return self.classes
+    
     def getDataset(self):
         return [self.tr_x, self.tr_y, self.ts_x, self.ts_y]
     
-    def getClasses(self):
-        return np.unique(np.concatenate((self.tr_y, self.ts_y), axis=0)).tolist()
+##  Example usage:
+# import datasets
+# from torchvision.transforms import ToTensor as toTensor
+# from torchvision import transforms
+# from torch.utils.data import DataLoader
+# from torch.utils.data import random_split
+# from torchvision.transforms import ToPILImage
+# import matplotlib.pyplot as plt
+# import torch
+# from PIL import Image
 
-# # Example usage:
-# csv_file = 'data/cifar100_nl.csv'  # Path to your CIFAR-100 NL test CSV file
-# loader = C100Dataset("data/cifar100_nl.csv", 'data/cifar100_nl_test.csv')
-# [data_nl_tr_x, data_nl_tr_y, data_nl_val_x, data_nl_val_y] = loader.getDataset()
+# transform = transforms.Compose([
+#     transforms.Resize((32,32)),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.RandomRotation(10),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
+#     # Using previous information, skipping manual calculation
+# ])
+
+# target_transform = transforms.Compose([
+#     transforms.ToTensor()
+# ])
+
+# trainDataset = datasets.C100Dataset('dataset/data/cifar100_nl.csv', 
+#                                     root_dir="dataset",
+#                                     train=True,
+#                                     transform= transform)
+
+# testDataset = datasets.C100Dataset('dataset/data/cifar100_nl_test.csv',
+#                                     root_dir="dataset",
+#                                     test=True,
+#                                     transform = transform,
+#                                     target_transform=target_transform)
+
+# trainDataset, valDataset = random_split(trainDataset, [.8, .2])
+
+# trainLoader = DataLoader(trainDataset, batch_size=batch_size, shuffle=True)
+# valLoader = DataLoader(valDataset, batch_size=batch_size, shuffle=True)
+# testLoader = DataLoader(testDataset, batch_size=batch_size, shuffle=True)
